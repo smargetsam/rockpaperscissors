4dd88fcc52220b775e57f532b32ffc6a6d460bca
smargetsam <95829951+smargetsam@users.noreply.github.com>
2023-05-23 15:15:21 +1000

    added main logic to play rps in command line taking input from modal

--- myscripts.js
+++ myscripts.js
@@ -5,6 +5,41 @@ function getComputerChoice(){
   return num
 }
 
+function generateCompRPS(){
+    let choice = getComputerChoice()
+    switch (choice){
+        case 1:
+            return "rock"
+            break;
+        case 2:
+            return "scissors"
+            break;
+        case 3:
+            return "paper"
+            break;
+    }
+}
+
+function getUserChoice(){
+    let choice = prompt("What is your response to ROCK-PAPER-SCISSORS-SHOOT?")
+    switch(choice.toLowerCase()){
+        case "rock":
+        return "rock"
+        break;
+        
+        case "paper":
+        return "paper"
+        break;
+
+        case "scissors":
+        return "scissors"
+        break;
+        default:
+            console.log("that was invalid, try again")
+            getUserChoice();
+}
+}
+
 function getRandomVal(max, min) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -12,4 +47,24 @@ function getRandomVal(max, min) {
   }
   
 
-  console.log(getComputerChoice())
+function game(compchoice, playerchoice){
+    let play = (compchoice+playerchoice)
+    
+    switch(play){
+        case "rockpaper":
+            case "scissorsrock":
+                case "paperscissors":
+                console.log("you win! The computer chose " + compchoice + " and your choice of " + playerchoice + " beats " + compchoice)
+            break;
+        case "rockrock":
+            case "paperpaper":
+                case "scissorsscissors":
+                console.log("The computer also chose " + compchoice + ". Its a draw! refresh to play again.")
+                break;
+        default:
+        console.log("you lost! The computer chose " + compchoice + " which beats your choice of  " + playerchoice)
+    } 
+
+}
+
+  console.log(game(generateCompRPS(),getUserChoice()))

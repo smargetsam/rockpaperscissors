e48901337c76a2ef52a27d77683346bf38327611
smargetsam <95829951+smargetsam@users.noreply.github.com>
2023-05-23 15:18:38 +1000

    formatted indentation for cleanliness and readbility

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,13 @@
 //build a game that plays rock papers scissors
 
-function getComputerChoice(){
-  let num = getRandomVal(3,1)
-  return num
+function getComputerChoice() {
+    let num = getRandomVal(3, 1)
+    return num
 }
 
-function generateCompRPS(){
+function generateCompRPS() {
     let choice = getComputerChoice()
-    switch (choice){
+    switch (choice) {
         case 1:
             return "rock"
             break;
@@ -20,51 +20,51 @@ function generateCompRPS(){
     }
 }
 
-function getUserChoice(){
+function getUserChoice() {
     let choice = prompt("What is your response to ROCK-PAPER-SCISSORS-SHOOT?")
-    switch(choice.toLowerCase()){
+    switch (choice.toLowerCase()) {
         case "rock":
-        return "rock"
-        break;
-        
+            return "rock"
+            break;
+
         case "paper":
-        return "paper"
-        break;
+            return "paper"
+            break;
 
         case "scissors":
-        return "scissors"
-        break;
+            return "scissors"
+            break;
         default:
             console.log("that was invalid, try again")
             getUserChoice();
-}
+    }
 }
 
 function getRandomVal(max, min) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-  
+}
 
-function game(compchoice, playerchoice){
-    let play = (compchoice+playerchoice)
-    
-    switch(play){
+
+function game(compchoice, playerchoice) {
+    let play = (compchoice + playerchoice)
+
+    switch (play) {
         case "rockpaper":
-            case "scissorsrock":
-                case "paperscissors":
-                console.log("you win! The computer chose " + compchoice + " and your choice of " + playerchoice + " beats " + compchoice)
+        case "scissorsrock":
+        case "paperscissors":
+            console.log("you win! The computer chose " + compchoice + " and your choice of " + playerchoice + " beats " + compchoice)
             break;
         case "rockrock":
-            case "paperpaper":
-                case "scissorsscissors":
-                console.log("The computer also chose " + compchoice + ". Its a draw! refresh to play again.")
-                break;
+        case "paperpaper":
+        case "scissorsscissors":
+            console.log("The computer also chose " + compchoice + ". Its a draw! refresh to play again.")
+            break;
         default:
-        console.log("you lost! The computer chose " + compchoice + " which beats your choice of  " + playerchoice)
-    } 
+            console.log("you lost! The computer chose " + compchoice + " which beats your choice of  " + playerchoice)
+    }
 
 }
 
-  console.log(game(generateCompRPS(),getUserChoice()))
+console.log(game(generateCompRPS(), getUserChoice()))
